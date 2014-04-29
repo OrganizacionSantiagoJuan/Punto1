ae0d604c0537fb08750c53dc052c8b83a18f428b
Juan Camilo Castro <jccastrop2012@gmail.com>
2014-04-29 00:55:34 -0500

    Commit en rama principal

---
+++ Punto1.java
@@ -0,0 +1,17 @@
+
+
+public class Punto1 {   
+      
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        
+        System.out.println("Hola Mundo");      
+        
+
+    }
+
+}
